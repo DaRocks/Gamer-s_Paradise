3bf39b93aab6a4ab33680a80e3ae7462d247f248
Dheeraj <52129100+Dheer001@users.noreply.github.com>
2023-11-19 15:28:16 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let secItem = document.querySelector("#sec");
 
     // Date CountDown 
 let countDown = () => {
-    let futureDate = new Date("20 November 2024");
+    let futureDate = new Date("20 January 2024");
     let currentDate = new Date();
     let myDate = futureDate - currentDate;
 
